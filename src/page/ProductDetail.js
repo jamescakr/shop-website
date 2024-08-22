7ac38302003903d364ac7217a983effcfb6b0296
jamesjo <jamescakr@gmail.com>
2024-08-22 11:02:01 -0600

    construction page updated

--- src/page/ProductDetail.js
+++ src/page/ProductDetail.js
@@ -1,7 +1,12 @@
 import React from "react";
 
 const ProductDetail = () => {
-  return <div>상품 상세 페이지</div>;
+  return (
+    <div>
+      <h1>현재 웹페이지 공사중입니다. 하루만 더 참아주세요!</h1>
+      <img src="images/construction.webp" />
+    </div>
+  );
 };
 
 export default ProductDetail;

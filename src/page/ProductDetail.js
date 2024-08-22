13f27c3e456546241e072df223184c04ea9e98c0
jamesjo <jamescakr@gmail.com>
2024-08-22 11:12:38 -0600

    construction added

--- src/page/ProductDetail.js
+++ src/page/ProductDetail.js
@@ -2,9 +2,10 @@ import React from "react";
 
 const ProductDetail = () => {
   return (
-    <div>
-      <h1>현재 웹페이지 공사중입니다. 하루만 더 참아주세요!</h1>
-      <img src="images/construction.webp" />
+    <div style={{ display: "flex" }}>
+      <h2>현재 웹페이지 공사중입니다. 하루만 더 참아주세요!</h2>
+
+      <img src="/images/construction.webp" width={500} />
     </div>
   );
 };
